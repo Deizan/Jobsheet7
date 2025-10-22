a542919db4a11daaecb6ab26cf8183bf64d0557f
Deizan <deizanfakhri7@gmail.com>
2025-10-22 17:25:46 +0700

    First commit

---
+++ SiakadFOR16.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class SiakadFOR16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai, tertinggi = 0, terendah = 100;
+        int i;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jml = sc.nextInt();
+        
+        for ( i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+           
+        }
+
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        
+        sc.close();
+    }
+}
